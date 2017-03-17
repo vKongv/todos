18b698dbc93630bec26a99b53beac95f16f3ee7e
Kong <kwkong@seekasia.com>
2017-03-17 11:45:02 +0800

    Code for lesson 5

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,16 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './App';
 
+// v4 react-router no longer support browserHistory, use this instead
+const history = createBrowserHistory();
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>
 );
 
--- src/configureStore.js
+++ src/configureStore.js
@@ -4,18 +4,18 @@ import { loadState, saveState } from './localStorage';
 import throttle from 'lodash/throttle';
 
 const configureStore = () => {
-    const persistedState = loadState();
-    const store = createStore(
-        todoApp,
-        persistedState
-    );
-    store.subscribe(throttle(() => {
-        saveState({
-            todos: store.getState().todos,
-        });
-    }, 1000));
+  const persistedState = loadState();
+  const store = createStore(
+    todoApp,
+    persistedState
+  );
+  store.subscribe(throttle(() => {
+    saveState({
+      todos: store.getState().todos,
+    });
+  }, 1000));
 
-    return store;
+  return store;
 };
 
 
