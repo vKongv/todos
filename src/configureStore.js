742e6aa84dcf6c50da5451d9c24e165083d3be39
Kong <kwkong@seekasia.com>
2017-03-29 17:43:28 +0800

    - Enhance middlewares apply to dispatch function

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,30 +3,32 @@ import todoApp from './reducers';
 import { loadState, saveState } from './localStorage';
 import throttle from 'lodash/throttle';
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
+const logger = (store) => (next) => {
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
   return (action) => {
     console.group(action.type);
     console.log('%c prev state', 'color: gray', store.getState());
     console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log('%c next state', 'color: green', store.getState());
     console.groupEnd(action.type);
     return returnValue;
   };
 };
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
+const promise = () => (next) => (action) => {
     if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
+      return action.then(next);
     }
-    return rawDispatch(action);
+    return next(action);
   };
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => // eslint-disable-line
+    store.dispatch = middleware(store)(store.dispatch) // eslint-disable-line
+  );
 };
 
 const configureStore = () => {
@@ -38,11 +40,13 @@ const configureStore = () => {
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   /* eslint-enable */
+  const middlewares = [promise];
+
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store);
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   store.subscribe(throttle(() => {
     saveState({
