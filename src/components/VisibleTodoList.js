1b57549dc9b12ad05cbac32da0187138130e0b02
Kong <kwkong@seekasia.com>
2017-03-29 14:09:30 +0800

    -Fetch and log todo data from fake backend when route change

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,19 +1,47 @@
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { toggleTodo, deleteTodo } from '../actions';
 import { withRouter } from 'react-router';
 import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
+import { fetchTodos } from '../api';
 
-const mapStateToProps = (state, { match }) => ({
-    todos: getVisibleTodos(state, match.params.filter || 'all'),
-});
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos) // eslint-disable-line no-console
+    );
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos =>
+        console.log(this.props.filter, todos) // eslint-disable-line no-console
+      );
+    }
+  }
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
 
-const VisibleTodoList = withRouter(connect(
+VisibleTodoList.propTypes = {
+  filter: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state, { match }) => {
+  const filter = match.params.filter || 'all';
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter,
+  };
+};
+
+VisibleTodoList = withRouter(connect(
   mapStateToProps,
   {
     onTodoClick: toggleTodo,
     onTodoDelete: deleteTodo,
   }
-)(TodoList));
+)(VisibleTodoList));
 
 export default VisibleTodoList;
