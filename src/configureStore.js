8376b488e2519c8289c928263133478c5e2ed04a
Kong <kwkong@seekasia.com>
2017-03-29 13:25:31 +0800

    - Add in logger for action and state

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,22 @@ import todoApp from './reducers';
 import { loadState, saveState } from './localStorage';
 import throttle from 'lodash/throttle';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
   /* eslint-disable no-underscore-dangle */
@@ -12,6 +28,11 @@ const configureStore = () => {
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   /* eslint-enable */
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
+
   store.subscribe(throttle(() => {
     saveState({
       todos: store.getState().todos,
