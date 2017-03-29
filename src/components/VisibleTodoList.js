00dd42b28e3d434ef416c92eaae6189449a85e53
Kong <kwkong@seekasia.com>
2017-03-29 10:54:58 +0800

    - mapDispatchToProps Shorthand

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -8,18 +8,21 @@ const mapStateToProps = (state, { match }) => ({
     todos: getVisibleTodos(state, match.params.filter || 'all'),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    onTodoClick: (id) => {
-      dispatch(toggleTodo(id));
-    },
-    onTodoDelete: (id) => {
-      dispatch(deleteTodo(id));
-    },
-});
+// const mapDispatchToProps = (dispatch) => ({
+//     onTodoClick: (id) => {
+//       dispatch(toggleTodo(id));
+//     },
+//     onTodoDelete: (id) => {
+//       dispatch(deleteTodo(id));
+//     },
+// });
 
 const VisibleTodoList = withRouter(connect(
   mapStateToProps,
-  mapDispatchToProps
+  {
+    onTodoClick: toggleTodo,
+    onTodoDelete: deleteTodo,
+  }
 )(TodoList));
 
 export default VisibleTodoList;
